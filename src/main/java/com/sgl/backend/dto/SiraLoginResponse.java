66aef501cfd587f1d7a3752f46e37c393de786af
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2025-10-08 23:33:56 -0500

    Add DTO classes for login requests and responses

---
+++ src/main/java/com/sgl/backend/dto/SiraLoginResponse.java
@@ -0,0 +1,15 @@
+package com.sgl.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SiraLoginResponse {
+    private String token;
+    private boolean isStudent;
+}
