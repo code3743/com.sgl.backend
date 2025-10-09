2a86584cf571c51a14b94276b41030532d9096f7
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2025-10-09 09:37:53 -0500

    Refactor BackendApplicationTests to specify active test profile in SpringBootTest annotation

--- src/test/java/com/sgl/backend/BackendApplicationTests.java
+++ src/test/java/com/sgl/backend/BackendApplicationTests.java
@@ -3,12 +3,9 @@ package com.sgl.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// Test to ensure the Spring application context loads successfully
-	}
-
+    @Test
+    void contextLoads() { }
 }
